2d211e4f0c0db2c1af9d337a1f257d20ab8451ab
Jerome <jerome.thai.87@gmail.com>
2016-02-17 15:50:46 -0800

    modified Frank-Wolfe with heterogeneous solver

--- frank_wolfe.py
+++ frank_wolfe.py
@@ -2,9 +2,7 @@ __author__ = "Jerome Thai"
 __email__ = "jerome.thai@berkeley.edu"
 
 import numpy as np
-from scipy.sparse import csr_matrix
 from All_Or_Nothing import all_or_nothing
-from scipy import special as sp
 
 
 def potential(graph ,f):
@@ -67,108 +65,54 @@ def solver_2(graph, demand, max_iter=100, eps=1e-8):
 
         # flow update
         L = all_or_nothing(g, demand)
-        # def h(a): return compute_potential(graph, (1.-a)*f + a*L)
         s = line_search(lambda a: potential(graph, (1.-a)*f+a*L)) if i>max_iter-10 \
             else 2./(i+2.)
         #s = line_search(lambda a: potential(graph, (1.-a)*f+a*L)) if i%10==9 \
         #    else 2./(i+2.)
         if s < eps: return f
-        # print 'line_search', s
-        # print h(s-1e-3), h(s), h(s+1e-3) 
         f = (1.-s) * f + s * L
     return f
 
 
-def solver_3(graph, demand, q=5, max_iter=100):
+def solver_3(graph, demand, q=5, max_iter=100, eps=1e-8):
     # modified Frank-Wolfe from Masao Fukushima
     links = int(np.max(graph[:,0])+1)
     f = np.zeros(links,dtype="float64") # initial flow assignment is null
     L = np.zeros(links,dtype="float64")
+    fs = np.zeros((links,q),dtype="float64")
     g = np.copy(graph[:,:4])
+
     for i in range(max_iter):
         # construct weighted graph with latest flow assignment
         x = np.power(f.reshape((links,1)), np.array([0,1,2,3,4]))
-        g[:,3] = np.einsum('ij,ij->i', x, graph[:,3:])
-        # flow update
-        f[:,i%q] = all_or_nothing(g, demand)
-        v = np.sum(f,1) / min(q,i+1) - f
-
-
-def gauss_seidel(graphs, demands, solver, max_cycles=10, max_iter=100, by_origin=False):
-    # we are given a list of graphs and demands that are specific for different types of players
-    # the gauss-seidel scheme updates cyclically for each type at a time
-
-    # prepare arrays for assignment by type
-    types = len(graphs)
-    links = int(np.max(graphs[0][:,0])+1)
-    fs = np.zeros((links,types),dtype="float64")
-    g = np.copy(graphs[0])
-
-    for cycle in range(max_cycles):
-        for i in range(types):
-            # construct graph with updated latencies
-            shift = np.sum(fs[:,range(i)+range(i+1,types)], axis=1)
-            shift_graph(graphs[i], g, shift)
-            # update flow assignment for this type
-            fs[:,i] = solver(g, demands[i], max_iter)
-    return fs
-
-
-def jacobi(graphs, demands, solver, max_cycles=10, max_iter=100, by_origin=False):
-    # given a list of graphs and demands specific for different types of players
-    # the jacobi scheme updates simultenously for each type at the same time
-
-    # prepare arrays for assignment by type
-    types = len(graphs)
-    links = int(np.max(graphs[0][:,0])+1)
-    fs = np.zeros((links,types),dtype="float64")
-    updates = np.copy(fs)
-    g = np.copy(graphs[0])
-
-    for cycle in range(max_cycles):
-        for i in range(types):
-            # construct graph with updated latencies
-            shift = np.sum(fs[:,range(i)+range(i+1,types)], axis=1)
-            shift_graph(graphs[i], g, shift)
-            # update flow assignment for this type
-            updates[:,i] = solver(g, demands[i], max_iter)
-        # batch update
-        fs = np.copy(updates)
-    return fs
-
-
-def shift_graph(graph1, graph2, d):
-    # given a graph with polynomial latency functions sum_k a_k x^k and shift d
-    # return a graph with updated latencies sum_k a_k (x+d)^k
-    links = graph1.shape[0]
-    for i in range(links):
-        graph2[i,3:] = shift_polynomial(graph1[i,3:], d[i])
-    return graph2
-
-
-def shift_polynomial(coef, d):
-    # given polynomial sum_k a_k x^k -> sum_k a_k (x+d)^k
-    coef2 = np.zeros(5,dtype="float64")
-    for i in range(5):
-        for j in range(i,5):
-            coef2[i] = coef2[i] + coef[j] * (d**(j-i)) * sp.binom(j,i)
-    return coef2
-
-
-# def shift_graph_2(graph1, graph2, d):
-#     # numpy matrix implementation of shift_graph
-#     A = np.zeros((5,5),dtype="float64")
-#     for i in range(5):
-#         for j in range(i,5):
-#             A[j,i] = (d**(j-i)) * sp.binom(j,i)
-#     graph2[:,3:] = graph2[:,3:].dot(A)
-#     return graph2
+        grad = np.einsum('ij,ij->i', x, graph[:,3:])
+        g[:,3] = grad
+        L = all_or_nothing(g, demand)
+        fs[:,i%q] = L
+        w = L - f
+        if np.linalg.norm(w) < eps: return f
+        if i > max_iter-10:
+            # step 3 of Fukushima
+            v = np.sum(fs,1) / min(q,i+1) - f
+            if np.linalg.norm(v) < eps: return f
+            # step 4 of Fukushima
+            gamma_1 = grad.dot(v) / np.linalg.norm(v)
+            gamma_2 = grad.dot(w) / np.linalg.norm(w)
+            if gamma_2 > -eps: return f
+            d = v if gamma_1 < gamma_2 else w
+            # step 5 of Fukushima
+            s = line_search(lambda a: potential(graph, f+a*d))
+            if s < eps: return f
+            f = f + s*d
+        else:
+            f = f + 2*w/(i+2.)
+    return f
 
 
 def main():
     graph = np.loadtxt('data/braess_net.csv', delimiter=',', skiprows=1)
     demand = np.loadtxt('data/braess_od.csv', delimiter=',', skiprows=1)
-    f = equilibrium_solver(graph, demand)
+    f = solver(graph, demand)
     print f
 
 
---
+++ heterogeneous_solver.py
@@ -0,0 +1,76 @@
+__author__ = "Jerome Thai"
+__email__ = "jerome.thai@berkeley.edu"
+
+import numpy as np
+from scipy import special as sp
+
+
+def gauss_seidel(graphs, demands, solver, max_cycles=10, max_iter=100, by_origin=False):
+    # we are given a list of graphs and demands that are specific for different types of players
+    # the gauss-seidel scheme updates cyclically for each type at a time
+
+    # prepare arrays for assignment by type
+    types = len(graphs)
+    links = int(np.max(graphs[0][:,0])+1)
+    fs = np.zeros((links,types),dtype="float64")
+    g = np.copy(graphs[0])
+
+    for cycle in range(max_cycles):
+        for i in range(types):
+            # construct graph with updated latencies
+            shift = np.sum(fs[:,range(i)+range(i+1,types)], axis=1)
+            shift_graph(graphs[i], g, shift)
+            # update flow assignment for this type
+            fs[:,i] = solver(g, demands[i], max_iter)
+    return fs
+
+
+def jacobi(graphs, demands, solver, max_cycles=10, max_iter=100, by_origin=False):
+    # given a list of graphs and demands specific for different types of players
+    # the jacobi scheme updates simultenously for each type at the same time
+
+    # prepare arrays for assignment by type
+    types = len(graphs)
+    links = int(np.max(graphs[0][:,0])+1)
+    fs = np.zeros((links,types),dtype="float64")
+    updates = np.copy(fs)
+    g = np.copy(graphs[0])
+
+    for cycle in range(max_cycles):
+        for i in range(types):
+            # construct graph with updated latencies
+            shift = np.sum(fs[:,range(i)+range(i+1,types)], axis=1)
+            shift_graph(graphs[i], g, shift)
+            # update flow assignment for this type
+            updates[:,i] = solver(g, demands[i], max_iter)
+        # batch update
+        fs = np.copy(updates)
+    return fs
+
+
+def shift_graph(graph1, graph2, d):
+    # given a graph with polynomial latency functions sum_k a_k x^k and shift d
+    # return a graph with updated latencies sum_k a_k (x+d)^k
+    links = graph1.shape[0]
+    for i in range(links):
+        graph2[i,3:] = shift_polynomial(graph1[i,3:], d[i])
+    return graph2
+
+
+def shift_polynomial(coef, d):
+    # given polynomial sum_k a_k x^k -> sum_k a_k (x+d)^k
+    coef2 = np.zeros(5,dtype="float64")
+    for i in range(5):
+        for j in range(i,5):
+            coef2[i] = coef2[i] + coef[j] * (d**(j-i)) * sp.binom(j,i)
+    return coef2
+
+
+# def shift_graph_2(graph1, graph2, d):
+#     # numpy matrix implementation of shift_graph
+#     A = np.zeros((5,5),dtype="float64")
+#     for i in range(5):
+#         for j in range(i,5):
+#             A[j,i] = (d**(j-i)) * sp.binom(j,i)
+#     graph2[:,3:] = graph2[:,3:].dot(A)
+#     return graph2
